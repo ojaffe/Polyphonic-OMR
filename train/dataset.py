fixed attention masking,

train/dataset.py
@@ -65,10 +65,7 @@ class PadCollate:
         imgs = torch.stack(list(imgs), dim=0)
         tgt = pad_sequence(tgt, batch_first=True, padding_value=self.PAD_IDX)
 
-         # Construct padding mask
-        tgt_pad_mask = tgt.ne(self.PAD_IDX)
-
-        return imgs, tgt, tgt_pad_mask
+        return imgs, tgt
 
 
 def create_tokenizer(data_cfg):
@@ -80,7 +77,7 @@ def create_tokenizer(data_cfg):
     idx2note = {0: "<SOS>", 1: "<EOS>", 2: "<PAD>", 3: "<DOT>", 4: "<CHORD START>", 5: "<CHORD END>"}
     initial_len = len(note2idx)
 
-    with open(data_cfg.get("vocab_path", None), 'r') as f:
+    with open(data_cfg.get("vocab_path"), 'r') as f:
         words = f.read().split('\n')
 
     for i, word in enumerate(words):
@@ -92,15 +89,15 @@ def create_tokenizer(data_cfg):
     return note2idx, idx2note, vocab_size, SOS_IDX, EOS_IDX, PAD_IDX, DOT_IDX
 
 
-def load_data(data_cfg):
+def load_data(data_cfg, seed):
     note2idx, idx2note, vocab_size, SOS_IDX, EOS_IDX, PAD_IDX, DOT_IDX = create_tokenizer(data_cfg)
 
     batch_size = data_cfg.get("batch_size", 4)
 
     # Create dataset from cleaned data, if doesn't already exist
-    csv_out = data_cfg.get("csv_out", None)
+    csv_out = data_cfg.get("csv_out")
     if not os.path.isfile(csv_out) or data_cfg.get("remake_csv", False):
-        make_csv(data_cfg)
+        make_csv(data_cfg, seed)
 
     dataset = PolyphonicDataset(data_cfg, note2idx, idx2note, vocab_size)
 

train/decoder/blocks/decoder_layer.py
@@ -14,11 +14,13 @@ class DecoderLayer(nn.Module):
 
     def __init__(self, d_model, ffn_hidden, n_head, drop_prob):
         super(DecoderLayer, self).__init__()
-        self.self_attention = MultiHeadAttention(d_model=d_model, n_head=n_head)
+        #self.self_attention = MultiHeadAttention(d_model=d_model, n_head=n_head)
+        self.self_attention = nn.MultiheadAttention(embed_dim=d_model, num_heads=n_head, batch_first=True)
         self.norm1 = LayerNorm(d_model=d_model)
         self.dropout1 = nn.Dropout(p=drop_prob)
 
-        self.enc_dec_attention = MultiHeadAttention(d_model=d_model, n_head=n_head)
+        #self.enc_dec_attention = MultiHeadAttention(d_model=d_model, n_head=n_head)
+        self.enc_dec_attention = nn.MultiheadAttention(embed_dim=d_model, num_heads=n_head, batch_first=True)
         self.norm2 = LayerNorm(d_model=d_model)
         self.dropout2 = nn.Dropout(p=drop_prob)
 
@@ -26,10 +28,11 @@ class DecoderLayer(nn.Module):
         self.norm3 = LayerNorm(d_model=d_model)
         self.dropout3 = nn.Dropout(p=drop_prob)
 
-    def forward(self, dec, enc, t_mask, s_mask):
+    def forward(self, enc, dec, dec_pad_mask, dec_causal_mask):
         # 1. compute self attention
         _x = dec
-        x = self.self_attention(q=dec, k=dec, v=dec, mask=t_mask)
+
+        x, _ = self.self_attention(query=dec, key=dec, value=dec, key_padding_mask=dec_pad_mask, is_causal=True)
         
         # 2. add and norm
         x = self.dropout1(x)
@@ -38,7 +41,7 @@ class DecoderLayer(nn.Module):
         if enc is not None:
             # 3. compute encoder - decoder attention
             _x = x
-            x = self.enc_dec_attention(q=x, k=enc, v=enc, mask=s_mask)
+            x, _ = self.enc_dec_attention(query=x, key=enc, value=enc)
             
             # 4. add and norm
             x = self.dropout2(x)

train/decoder/model/decoder.py
@@ -27,12 +27,12 @@ class Decoder(nn.Module):
 
         self.linear = nn.Linear(d_model, dec_voc_size)
 
-    def forward(self, trg, enc_src, trg_mask, src_mask):
-        trg = self.emb(trg)
+    def forward(self, enc_src, tgt, tgt_pad_mask, tgt_causal_mask):
+        tgt = self.emb(tgt)
 
         for layer in self.layers:
-            trg = layer(trg, enc_src, trg_mask, src_mask)
+            tgt = layer(enc_src, tgt, tgt_pad_mask, tgt_causal_mask)
 
         # pass to LM head
-        output = self.linear(trg)
+        output = self.linear(tgt)
         return output

train/decoder/model/transformer.py
@@ -25,35 +25,6 @@ class Transformer(nn.Module):
                                n_layers=n_layers,
                                device=device)
 
-    def forward(self, im_emb, tgt, tgt_pad_mask):
-        tgt_mask = None
-        if self.training:
-            tgt_mask = self.make_pad_mask(tgt_pad_mask, tgt_pad_mask) * \
-                    self.make_no_peak_mask(tgt_pad_mask, tgt_pad_mask)
-
-        output = self.decoder(tgt, im_emb, tgt_mask, None)
+    def forward(self, im_emb, tgt, tgt_pad_mask, tgt_causal_mask):
+        output = self.decoder(im_emb, tgt, tgt_pad_mask, tgt_causal_mask)
         return output
-
-    def make_pad_mask(self, q, k):
-        len_q, len_k = q.size(1), k.size(1)
-
-        # batch_size x 1 x 1 x len_k
-        k = k.unsqueeze(1).unsqueeze(2)
-        # batch_size x 1 x len_q x len_k
-        k = k.repeat(1, 1, len_q, 1)
-
-        # batch_size x 1 x len_q x 1
-        q = q.unsqueeze(1).unsqueeze(3)
-        # batch_size x 1 x len_q x len_k
-        q = q.repeat(1, 1, 1, len_k)
-
-        mask = k & q
-        return mask
-
-    def make_no_peak_mask(self, q, k):
-        len_q, len_k = q.size(1), k.size(1)
-
-        # len_q x len_k
-        mask = torch.tril(torch.ones(len_q, len_k)).type(torch.BoolTensor).to(self.device)
-
-        return mask

train/encoder.py
@@ -56,7 +56,7 @@ class Block(nn.Module):
 
 
 class EncoderCRNN(nn.Module):
-    def __init__(self, img_channels, layers, no_lstm_layers):
+    def __init__(self, img_channels, layers, no_lstm_layers, lstm_hidden):
         super(EncoderCRNN, self).__init__()
                 
         # Initial conv layer to increase channels
@@ -83,7 +83,7 @@ class EncoderCRNN(nn.Module):
 
         # Bi-LSTM encoding
         self.bi_lstm = nn.LSTM(input_size=512,
-                               hidden_size=256,
+                               hidden_size=int(lstm_hidden / 2),
                                num_layers=no_lstm_layers,
                                bias=False,
                                batch_first=True,
@@ -138,3 +138,50 @@ class EncoderCRNN(nn.Module):
         x, (h, c) = self.bi_lstm(x)  # (b, 112, 512) -> (b, 112, 1024)
 
         return x
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, vocab_size, d_model, n_head, max_len,
+                 ffn_hidden, n_layers, drop_prob, device):
+        super(Decoder, self).__init__()
+
+        self.pos_encoder = PositionalEncoding(d_model, dropout=drop_prob)
+        self.encoder = nn.Embedding(vocab_size, d_model)
+
+        decoder_layer = nn.TransformerDecoderLayer(d_model=d_model, nhead=n_head, dim_feedforward=ffn_hidden,
+                                                   dropout=drop_prob, batch_first=True)
+        self.decoder = nn.TransformerDecoder(decoder_layer, num_layers=n_layers)
+        
+        self.lm_head = nn.Linear(d_model, vocab_size)
+        
+    def forward(self, im_emb, tgt_input, tgt_pad_mask, tgt_causal_mask):
+        emb = self.encoder(tgt_input)
+        if not self.train() and tgt_causal_mask is not None:
+            tgt_causal_mask = tgt_causal_mask[0]
+            
+        output = self.decoder(emb, im_emb, tgt_mask=tgt_causal_mask, tgt_key_padding_mask=tgt_pad_mask)
+        output = self.lm_head(output)
+
+        return output

train/make_csv.py
@@ -57,7 +57,7 @@ def strip_series(input: str, max_chord_stack: int) -> (list, list):
     return output, density
 
 
-def make_csv(data_cfg: dict) -> None:
+def make_csv(data_cfg: dict, seed) -> None:
     """
     Given an input directory and output path, for each .png and .semantic pair
     within the directory, write a row in an output csv of the .png path, and
@@ -88,22 +88,26 @@ def make_csv(data_cfg: dict) -> None:
             contents = f_sem.read()
 
         tokens, density = strip_series(contents, max_chord_stack)
-        all_densities.append(density)
+        
+        if len(tokens) + 2 <= data_cfg.get("max_seq_len"):  # +2 for SOS, EOS
+            data["img_path"].append(img_path)
+            data["tokens"].append(tokens)
 
-        data["img_path"].append(img_path)
-        data["tokens"].append(tokens)
+            all_densities.append(density)
 
     all_densities_sorted = list(reversed(np.argsort(all_densities)))
 
     df = pd.DataFrame.from_dict(data)
 
     # Genetate separate "hard" dataset
+    samples = 10000
     if data_cfg["gen_hard_dataset"]:
-        hard_idxs = all_densities_sorted[:1000]
+        hard_idxs = all_densities_sorted[:samples]
         df_easy = df.drop(hard_idxs)
         df_hard = df.loc[hard_idxs]
 
         df_easy.to_csv(csv_out, header=True, index=False)
         df_hard.to_csv(hard_csv_out, header=True, index=False)
     else:
-        df.to_csv(header=True, index=False)
+        df = df.sample(n=samples, random_state=seed)
+        df.to_csv(csv_out, header=True, index=False)

train/train.py
@@ -1,10 +1,11 @@
 import argparse
 import os
 import shutil
+import time
     
-from utils import set_seed, load_config, greedy_decode
+from utils import set_seed, load_config, greedy_decode, build_pad_mask, build_causal_mask, calc_acc
 from dataset import load_data
-from encoder import EncoderCRNN
+from encoder import EncoderCRNN, Decoder
 from decoder.model.transformer import Transformer
 from tqdm import tqdm
 
@@ -21,17 +22,30 @@ def train(cfg_file):
     log_dir = cfg["generic"].get("log_dir")
     if cfg['generic'].get('clear_log') and os.path.exists(log_dir):
         shutil.rmtree(log_dir)
-    writer = SummaryWriter(log_dir=log_dir)
+    run_log_dir = os.path.join(log_dir, cfg.get("name"), str(time.time()))
+    train_writer = SummaryWriter(log_dir=os.path.join(run_log_dir, "train"))
+    eval_writer = SummaryWriter(log_dir=os.path.join(run_log_dir, "eval"))
 
     set_seed(seed=cfg["generic"].get("seed"))
 
-    train_loader, val_loader, test_loader, note2idx, idx2note, vocab_size, SOS_IDX, EOS_IDX, PAD_IDX = load_data(data_cfg=cfg["data"])
+    train_loader, val_loader, test_loader, note2idx, idx2note, vocab_size, SOS_IDX, EOS_IDX, PAD_IDX = load_data(data_cfg=cfg["data"], seed=cfg["generic"].get("seed"))
 
-    encoder = EncoderCRNN(img_channels=1, layers=cfg["encoder"].get("layers"), no_lstm_layers=cfg["encoder"].get("no_lstm_layers")).to(device)
-    decoder = Transformer(vocab_size=vocab_size, 
+    encoder = EncoderCRNN(img_channels=1, 
+                          layers=cfg["encoder"].get("layers"), 
+                          no_lstm_layers=cfg["encoder"].get("no_lstm_layers"),
+                          lstm_hidden=cfg["decoder"].get("d_model")).to(device)
+    """decoder = Transformer(vocab_size=vocab_size, 
                           d_model=cfg["decoder"].get("d_model"), 
                           n_head=cfg["decoder"].get("n_head"), 
-                          max_len=cfg["decoder"].get("max_len"),
+                          max_len=cfg["data"].get("max_seq_len"),
+                          ffn_hidden=cfg["decoder"].get("ffn_hidden"), 
+                          n_layers=cfg["decoder"].get("n_layers"), 
+                          drop_prob=cfg["decoder"].get("drop_prob"), 
+                          device=device).to(device)"""
+    decoder = Decoder(vocab_size=vocab_size, 
+                          d_model=cfg["decoder"].get("d_model"), 
+                          n_head=cfg["decoder"].get("n_head"), 
+                          max_len=cfg["data"].get("max_seq_len"),
                           ffn_hidden=cfg["decoder"].get("ffn_hidden"), 
                           n_layers=cfg["decoder"].get("n_layers"), 
                           drop_prob=cfg["decoder"].get("drop_prob"), 
@@ -60,20 +74,21 @@ def train(cfg_file):
         encoder.train()
         decoder.train()
         for batch_idx, batch in tqdm(enumerate(train_loader)):
-            image, tgt, tgt_pad_mask = batch
-            image, tgt, tgt_pad_mask = image.to(device), tgt.to(device), tgt_pad_mask.to(device)
+            image, tgt = batch
+            image, tgt = image.to(device), tgt.to(device)
 
             tgt_input = tgt[:, :-1] # Remove last EOS
-            tgt_pad_mask = tgt_pad_mask[:, :-1]
-
             tgt_output = tgt[:, 1:]  # Remove first SOS
+
+            tgt_pad_mask = build_pad_mask(tgt_input, PAD_IDX).to(device)
+            tgt_causal_mask = build_causal_mask(tgt_input, cfg["decoder"].get("n_head"))
             
             loss = 0
             encoder_optimizer.zero_grad()
             decoder_optimizer.zero_grad()
 
             im_emb = encoder(image)
-            output = decoder(im_emb, tgt_input, tgt_pad_mask)
+            output = decoder(im_emb, tgt_input, tgt_pad_mask, tgt_causal_mask)
 
             loss = criterion(output.reshape(-1, output.shape[-1]), tgt_output.reshape(-1))
 
@@ -83,41 +98,52 @@ def train(cfg_file):
 
             # Tensorboard
             global_step = epoch*len(train_loader) + batch_idx
-            writer.add_scalar('Loss/train', loss, global_step)
+            train_writer.add_scalar('loss', loss, global_step)
+            train_writer.add_scalar('accuracy', calc_acc(output, tgt_output), global_step)
 
         # Decode example
         if decode and epoch % decode_every == 0:
             encoder.eval()
             decoder.eval()
-            tokens = greedy_decode(encoder, decoder, test_loader, PAD_IDX, SOS_IDX, EOS_IDX, device)
-            writer.add_text('Decoded/train', str(tokens), global_step)
+            tgt, tokens = greedy_decode(encoder, decoder, test_loader, PAD_IDX, SOS_IDX, EOS_IDX, device)
+
+            tgt_decoded = [idx2note[i] for i in tgt]
+            tokens_decoded = [idx2note[i] for i in tokens]
+            eval_writer.add_text('decoded', str(tokens_decoded), global_step)
+            eval_writer.add_text('decoded target', str(tgt_decoded), global_step)
 
         # Val
         val_loss = 0
+        val_acc = []
         encoder.eval()
         decoder.eval()
         for batch_idx, batch in tqdm(enumerate(val_loader)):
-            image, tgt, tgt_pad_mask = batch
-            image, tgt, tgt_pad_mask = image.to(device), tgt.to(device), tgt_pad_mask.to(device)
+            image, tgt = batch
+            image, tgt = image.to(device), tgt.to(device)
 
             tgt_input = tgt[:, :-1] # Remove last EOS
-            tgt_pad_mask = tgt_pad_mask[:, :-1]
-            
             tgt_output = tgt[:, 1:]  # Remove first SOS
+
+            tgt_pad_mask = build_pad_mask(tgt_input, PAD_IDX).to(device)
+            tgt_causal_mask = build_causal_mask(tgt_input, cfg["decoder"].get("n_head"))
             
             with torch.no_grad():
                 im_emb = encoder(image)
-                output = decoder(im_emb, tgt_input, tgt_pad_mask)
+                output = decoder(im_emb, tgt_input, tgt_pad_mask, tgt_causal_mask)
 
                 val_loss += criterion(output.reshape(-1, output.shape[-1]), tgt_output.reshape(-1))
+                val_acc.append(calc_acc(output, tgt_output))
 
         val_loss /= len(val_loader)
-        writer.add_scalar('Loss/val', val_loss, global_step)
+        eval_writer.add_scalar('loss', val_loss, global_step)
+        eval_writer.add_scalar('accuracy', sum(val_acc) / len(val_acc), global_step)
 
         # Save model
         if save and epoch % save_every == 0:
-            save_loc = os.path.join(save_dir, "model_epoch_{:}.pt".format(epoch))
-            torch.save(model.state_dict(), save_loc)
+            encoder_save_loc = os.path.join(save_dir, "encoder_epoch_{:}.pt".format(epoch))
+            decoder_save_loc = os.path.join(save_dir, "decoder_epoch_{:}.pt".format(epoch))
+            torch.save(encoder.state_dict(), encoder_save_loc)
+            torch.save(decoder.state_dict(), decoder_save_loc)
 
     writer.close()
 

train/utils.py
@@ -61,16 +61,14 @@ def greedy_decode(encoder, decoder, data_loader, PAD_IDX, SOS_IDX, EOS_IDX, devi
 
     # Get example
     batch = next(iter(data_loader))
-    image, tgt, _ = batch
+    image, tgt = batch
     
     image, tgt = image[0].unsqueeze(0).to(device), tgt[0].unsqueeze(0).to(device)
 
     with torch.no_grad():
+        im_emb = encoder(image)
         for i in tqdm(range(200)):
-            tgt_pad_mask = torch.ones((1, tgt_input.shape[1]), dtype=torch.int64).to(device)
-
-            im_emb = encoder(image)
-            output = decoder(im_emb, tgt_input, tgt_pad_mask)
+            output = decoder(im_emb, tgt_input, None, None)
 
             final_out = output[:, output.shape[1]-1, :]
             top_token = torch.argmax(final_out).item()
@@ -82,4 +80,31 @@ def greedy_decode(encoder, decoder, data_loader, PAD_IDX, SOS_IDX, EOS_IDX, devi
             if top_token == EOS_IDX:
                 break
 
-    return tgt_input.detach().tolist()[0]
+    return tgt[0].tolist(), tgt_input.detach().tolist()[0]
+
+
+def build_pad_mask(inputs, pad_idx):
+    pad_mask = (inputs == pad_idx).bool()
+
+    return pad_mask
+
+
+def build_causal_mask(inputs, num_heads):
+    batch_size, sequence_length = inputs.size()
+
+    causal_mask = torch.tril(torch.ones((sequence_length, sequence_length), dtype=torch.bool, device=inputs.device))
+    causal_mask = (causal_mask == 0)
+    causal_mask = causal_mask.repeat(batch_size * num_heads, 1, 1).bool()
+    return causal_mask
+
+
+def calc_acc(input, target):
+    """
+    :param input: [B, T, V]
+    :param target: [B, T]
+    """
+    input = input.softmax(-1)
+    categorical_input = input.argmax(-1)
+
+    bool_acc = categorical_input.long() == target.long()
+    return bool_acc.sum().to(torch.float) / bool_acc.numel()